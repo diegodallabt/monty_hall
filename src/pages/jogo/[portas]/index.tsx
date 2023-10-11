4698c6e0e962ccad2d29e5297c44b9d566cdf3d2
Diego D B Thedoldi <diegodallabt01@gmail.com>
2023-10-11 04:46:43 -0300

    Projeto Monty Hall com portas aleatórias

---
+++ src/pages/jogo/[portas]/index.tsx
@@ -0,0 +1,44 @@
+import styles from '../../../styles/Jogo.module.css'
+import { atualizarPortas, criarPortas } from '@/functions/portas';
+import Porta from '../../../components/Porta'; 
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+export default function Home() {
+  const router = useRouter()
+  const [portas, setPortas] = useState([])
+
+
+  
+  useEffect(() => {
+    const portas = +(router.query.portas ?? 3)
+    // gera um número aleatório mas que não pode ser 0
+    const temPresente = Math.floor(Math.random() * portas) + 1
+
+    setPortas(criarPortas(portas, temPresente))
+  }, [router?.query])
+
+
+  function renderizarPortas() {
+    return portas.map(porta => {
+      return <Porta key={porta.numero} value={porta} 
+      onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))}
+      />
+    })
+  }
+
+  return (
+    <div id={styles.jogo}>
+        <div className={styles.portas}>
+            {renderizarPortas()}
+        </div>
+        <div className={styles.botoes}>
+            <Link href="/">
+                <button>Reiniciar jogo</button>
+            </Link>
+        </div>
+    </div>
+    
+  )
+}
